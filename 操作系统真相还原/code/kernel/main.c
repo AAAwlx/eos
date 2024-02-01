2dd3db7516f116d9f53d19971e3397a6a53c9a58
AAAwlx <561639418@qq.com>
2024-02-01 11:17:01 +0800

    系统调用测试

--- 操作系统真相还原/code/kernel/main.c
+++ 操作系统真相还原/code/kernel/main.c
@@ -13,41 +13,40 @@ int main()
 {
     init_all();
     intr_enable();
+    console_put_str(" main_pid:0x");
+   console_put_int(sys_getpid());
+   console_put_char('\n');
     thread_start("k_thread_a", 31, k_thread_a, "argA ");
     thread_start("k_thread_b", 31, k_thread_b, "argB ");
    process_execute(u_prog_a, "user_prog_a");
    process_execute(u_prog_b, "user_prog_b");
-    while(1) {
-        printk("main\n");
-    };
+    while(1);
 }
 void k_thread_a(void* arg) {     
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
    char* para = arg;
-   while(1) {
-      printk("%s: %d\n", arg, test_var_a);
-   }
+   console_put_str(" thread_a_pid:0x");
+   console_put_int(sys_getpid());
+   console_put_char('\n');
+   while(1);
 }
 
 /* 在线程中运行的函数 */
 void k_thread_b(void* arg) {     
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
    char* para = arg;
-   while(1) {
-
-     printk("%s: %d\n", arg, test_var_b);
-      
-   }
+   console_put_str(" thread_b_pid:0x");
+   console_put_int(sys_getpid());
+   console_put_char('\n');
+   while(1);
 }
 void u_prog_a(void) {
-   while(1) {
-      test_var_a++;
-   }
+   printf("prog_a_pid:0x%x\n", getpid());
+   while(1);
 }
 
 /* 测试用户进程 */
 void u_prog_b(void) {
-   while(1) {
-      test_var_b++;
-   }
+   printf("prog_b_pid:0x%x\n", getpid());
+   while (1);
 }
